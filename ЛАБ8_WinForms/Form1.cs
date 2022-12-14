be0aeeead66dc2e6f0818eba80cd9b96309258ca
DaniilF2323@outlook.com <DaniilF2323@outlook.com>
2022-12-14 22:52:16 +0300

    Добавьте файлы проекта.

---
+++ ЛАБ8_WinForms/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ЛАБ8_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static IEnumerable<double> GetNumbers(string input)
+        {
+            var matches = Regex.Matches(input, @"-?\d+(?:\.\d+)?", RegexOptions.Compiled);
+            return from Match match in matches select double.Parse(match.Value, CultureInfo.InvariantCulture);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var s = textBox1.Text;
+            var nums = GetNumbers(s);
+            if (nums.Any())
+                textBox2.Text = nums.Max().ToString();
+            else
+                textBox2.Text = "Нет чисел!";
+        }
+    }
+}
---
+++ ЛАБ8_УП/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ЛАБ8_УП
+{
+    class Program
+    {
+        private static IEnumerable<double> GetNumbers(string input)
+        {
+            var matches = Regex.Matches(input, @"-?\d+(?:\.\d+)?", RegexOptions.Compiled);
+            return from Match match in matches select double.Parse(match.Value, CultureInfo.InvariantCulture);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше сообщение: ");
+            var s = Console.ReadLine();
+            var nums = GetNumbers(s);
+            if (nums.Any())
+                Console.WriteLine("Максимальное число в сообщении: {0}", nums.Max());
+            else
+                Console.WriteLine("В сообщении нет чисел!");
+        }
+    }
+}
